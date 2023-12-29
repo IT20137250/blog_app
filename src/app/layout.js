bc1fa5a9e86c3d899898f1ab9d5b8669d7298ecd
IT20137250 <it20137250@my.sliit.lk>
2023-12-29 15:53:10 +0530

    setup folder structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
+import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
+  title: 'RCP Blog App',
   description: 'The best blog app!',
 }
 
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className='container'>
           <div className='wrapper'>
+            <Navbar />
             {children}
+            <Footer />
           </div>
         </div>
       </body>
