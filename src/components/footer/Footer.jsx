83292eef0211c9dbcf56176cd6eaf01d585a2f46
IT20137250 <it20137250@my.sliit.lk>
2024-01-03 15:40:19 +0530

    create home page and mobile responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,57 @@
+import Image from 'next/image';
 import styles from './footer.module.css';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className={styles.container}>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image 
+            src='/logo.png'
+            alt='rcp blog'
+            width={50}
+            height={50}
+          />
+          <h1 className={styles.logoText}>RcpBlog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        </p>
+        <div className={styles.icons}>
+          <Image src='/facebook.png' alt='facebook' width={18} height={18} />
+          <Image src='/instagram.png' alt='instagram' width={18} height={18} />
+          <Image src='/tiktok.png' alt='tiktok' width={18} height={18} />
+          <Image src='/youtube.png' alt='youtube' width={18} height={18} />
+        </div>
+      </div>
+
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href='/'>Homepage</Link>
+          <Link href='/'>Blog</Link>
+          <Link href='/'>About</Link>
+          <Link href='/'>Contact</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href='/'>Homepage</Link>
+          <Link href='/'>Blog</Link>
+          <Link href='/'>About</Link>
+          <Link href='/'>Contact</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href='/'>Facebook</Link>
+          <Link href='/'>Instagram</Link>
+          <Link href='/'>Tiktok</Link>
+          <Link href='/'>Youtube</Link>
+        </div>
+      </div>
+    </div>
   )
 };
 
---
+++ src/components/menuPost/MenuPost.jsx
@@ -0,0 +1,103 @@
+import Link from 'next/link';
+import styles from './menuPost.module.css';
+import Image from 'next/image';
+
+const MenuPost = ({withImage}) => {
+    return (
+        <div className={styles.items}>
+            <Link href='/' className={styles.item}>
+                {withImage && (
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src='/p1.jpeg'
+                            alt='post image'
+                            fill
+                            className={styles.image}
+                        />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.travel}`}>
+                        Travel
+                    </span>
+                    <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipsicing elit.</h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>RCP Rajapaksha - </span>
+                        <span className={styles.date}>03.01.2024</span>
+                    </div>
+                </div>
+            </Link>
+
+            <Link href='/' className={styles.item}>
+                {withImage && (
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src='/p1.jpeg'
+                            alt='post image'
+                            fill
+                            className={styles.image}
+                        />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.culture}`}>
+                        Culture
+                    </span>
+                    <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipsicing elit.</h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>RCP Rajapaksha - </span>
+                        <span className={styles.date}>03.01.2024</span>
+                    </div>
+                </div>
+            </Link>
+
+            <Link href='/' className={styles.item}>
+                {withImage && (
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src='/p1.jpeg'
+                            alt='post image'
+                            fill
+                            className={styles.image}
+                        />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.food}`}>
+                        Food
+                    </span>
+                    <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipsicing elit.</h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>RCP Rajapaksha - </span>
+                        <span className={styles.date}>03.01.2024</span>
+                    </div>
+                </div>
+            </Link>
+
+            <Link href='/' className={styles.item}>
+                {withImage && (
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src='/p1.jpeg'
+                            alt='post image'
+                            fill
+                            className={styles.image}
+                        />
+                    </div>
+                )}
+                <div className={styles.textContainer}>
+                    <span className={`${styles.category} ${styles.fashion}`}>
+                        Fashion
+                    </span>
+                    <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipsicing elit.</h3>
+                    <div className={styles.detail}>
+                        <span className={styles.username}>RCP Rajapaksha - </span>
+                        <span className={styles.date}>03.01.2024</span>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+};
+
+export default MenuPost;
