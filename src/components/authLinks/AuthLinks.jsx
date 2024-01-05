f26fb403d07f41ffd8394f47966cbcb6789c2412
IT20137250 <it20137250@my.sliit.lk>
2024-01-05 09:23:34 +0530

    setup login and logout functions

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,20 +3,21 @@
 import Link from 'next/link';
 import styles from './authLinks.module.css';
 import { useState } from 'react';
+import { signOut, useSession } from 'next-auth/react';
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-  //temporary
-  const status = "notAuthenticated";
+
+  const { status } = useSession();
 
   return (
     <>
-      {status === "notAuthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href='/login' className={styles.link}>Login</Link>
       ) : (
         <>
             <Link href='/write' className={styles.link}>Write</Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>Logout</span>
         </>
       )}
 
